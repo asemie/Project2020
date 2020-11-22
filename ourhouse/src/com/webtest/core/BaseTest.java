7061c0b0f09c6ea74fadf7c2de851e03f4a36553
unknown <you@example.com>
2020-11-22 14:39:04 +0800

    UPDATE

---
+++ ourhouse/src/com/webtest/core/BaseTest.java
@@ -0,0 +1,110 @@
+package com.webtest.core;
+
+import static org.testng.Assert.assertTrue;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestContext;
+import org.testng.TestRunner;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+
+
+import com.webtest.utils.Log;
+import com.webtest.utils.ReadProperties;
+
+/**
+ * author:lihuanzhen
+ *
+ */
+
+
+public class BaseTest {
+
+	public  WebDriverEngine webtest;
+	private WebDriver driver;
+	public String driverType;
+	private WebDriver newWebDriver(String driverType) throws IOException {
+		WebDriver driver = null;
+	 if (driverType.equalsIgnoreCase("firefox")) {
+		    String firefox_driver =ReadProperties.getPropertyValue("gecko_driver");
+			String firefox_path = ReadProperties.getPropertyValue("firefox_path");
+			System.setProperty("webdriver.gecko.driver", firefox_driver);
+			System.setProperty("webdriver.firefox.bin", firefox_path);
+			driver = new FirefoxDriver();
+	
+			Log.info("Using Firefox");
+		}  else if (driverType.equalsIgnoreCase("chrome")) {
+			String chrome_path = ReadProperties.getPropertyValue("chrome_path");
+			System.setProperty("webdriver.chrome.driver",chrome_path);
+			driver = new ChromeDriver();
+			Log.info("Using Chrome");
+			
+		}else{
+			return null;
+		}	
+		return driver;
+	}
+
+
+	/**
+	 * 
+	 *打开浏览器
+	 * 
+	 */
+
+
+	@BeforeClass
+	public void doBeforeClass() throws Exception {
+
+		driverType=ReadProperties.getPropertyValue("driverType");
+		driver = this.newWebDriver(driverType);
+		driver.manage().window().maximize();
+		Log.info(driverType);
+		webtest = new WebDriverEngine(driver);
+		webtest.open(ReadProperties.getPropertyValue("base_url1"));
+		webtest.type("id=fm[uname]",ReadProperties.getPropertyValue("usname"));
+		webtest.type("id=fm[upass]",ReadProperties.getPropertyValue("upasswd"));
+		webtest.type("xpath=//input[@placeholder='认证码']",ReadProperties.getPropertyValue("ucode"));
+		webtest.click("xpath=//input[@value='提交']");
+		
+		webtest.click("xpath=//*[@id=\"adf_nav1\"]/a[5]");
+//		webtest.enterFrame("adf_main");
+		
+//		webtest.open("http://127.0.0.3/root/run/adm.php?dops-a&mod=album&part=house");
+//		webtest.open("http://127.0.0.3/root/run/adm.php?dops-a&mod=album&lpid=&part=house&view=form&stype=&recbk=ref");
+	}
+
+
+//	@AfterClass
+//	public void doAfterMethod() {
+//		if(this.driver != null){
+//			this.driver.quit();
+//			}
+//		Log.info("Quitted Browser");
+//	}
+//	
+	public WebDriver getDriver() {
+        return driver;
+    }
+	@BeforeSuite(description="添加监听器")
+	public void addListener(ITestContext context) {
+		TestRunner runner=(TestRunner)context;
+		runner.addListener(new com.webtest.freemarker.GenerateReporter());
+	}
+}
